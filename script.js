29590580adea27cf493ed803bcb0372647f265b6
Jason Eraste <113670429+Jeraste2k1@users.noreply.github.com>
2025-01-15 11:55:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 
 const menuIcon = document.querySelector('#menu-icon');
 const navLinks = document.querySelector('.nav-links');
-
- 
 menuIcon.onclick= () => {
   navLinks.classList.toggle('active');
 }
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const frBtn = document.getElementById("fr-btn");
+  const enBtn = document.getElementById("en-btn");
+
+  // Fonction pour changer la langue
+  function switchLanguage(lang) {
+    document.querySelectorAll("[data-fr]").forEach((element) => {
+      if (lang === "fr") {
+        element.textContent = element.getAttribute("data-fr");
+      } else if (lang === "en") {
+        element.textContent = element.getAttribute("data-en");
+      }
+    });
+  }
+
+  // Écouteurs pour les boutons
+  frBtn.addEventListener("click", () => switchLanguage("fr"));
+  enBtn.addEventListener("click", () => switchLanguage("en"));
+});
+
